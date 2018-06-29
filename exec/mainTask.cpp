3025ed8a7341c1cdd8fadca1ccf51f739f7bedec
HaowenLai <this_is_my_mail2@163.com>
2018-06-29 22:18:41 +0800

    class lettersClassify OK

--- exec/mainTask.cpp
+++ exec/mainTask.cpp
@@ -1,10 +1,15 @@
 /* *************************************************************
- * CAN communication example
- *   Must use `sudo` to run this program, and you should send
- * 8 byte each frame, or the mini stm32 may display abnormally.
- *   This is a multi-functional program, you can choose one of 
- * its functions by entering the corresponding number while 
- * running. It won't stop until the program is killed.
+ *          Main task that the robotic arm performs
+ *   The robotic arm will locate the target in three-dimensions.
+ * Then, it will move to the place roughly and adjust precisely
+ * by the feedback from camera attached to its clip. After that,
+ * it will turn the bottom surface of the target and let it face
+ * the front camera, and the program detects which letter it is.
+ * Finally, judged by different detections, the arm moves the
+ * target to corresponding places.
+ *
+ * @Author : Derek Lai (LHW)
+ * @Date   : 2018/6/29
  * ************************************************************/
 
 #include "UsbCAN.hpp"
@@ -17,200 +22,10 @@
 #include <stdio.h>
 #include <iostream>
 
-
 using namespace std;
 using namespace cv;
 
-//multi-functional. Functions to choose
-void swingPeriodically(UsbCAN& canII);
-void singleMove(UsbCAN& canII);
-void receiveFromCAN(UsbCAN& canII);
-void matlabPredict(UsbCAN& canII);
-void tfPredict(UsbCAN& canII);
-void moveInRoute(UsbCAN& canII);
-
-int main()
-{
-    UsbCAN canII;
-    if(canII.initCAN(UsbCAN::BAUDRATE_500K))
-    {
-        cout<<"init successfully\n"
-            <<"transmitting..."<<endl;
-    }
-
-    cout<< "Enter the number to choose a function:\n"
-           "1. swingPeriodically\n"
-           "2. singleMove\n"
-           "3. receiveFromCAN\n"
-           "4. matlabPredict\n"
-           "5. tfPredict\n"
-           "6. moveInRoute\n";
-    
-    int choose = 1;
-    cin >> choose;
-    switch(choose)
-    {
-      case 1:
-        swingPeriodically(canII);
-        break;
-      case 2:
-        singleMove(canII);
-        break;
-      case 3:
-        receiveFromCAN(canII);
-        break;
-      case 4:
-        matlabPredict(canII);
-        break;
-      case 5:
-        tfPredict(canII);
-        break;
-      case 6:
-        moveInRoute(canII);
-        break;
-      default:
-        cout<<"your choice is not in the range! Exit.\n";
-        break;
-    }
-    
-    return 0;
-}
-
-
-//This function will block the program until it ends
-//It make the robotic arm1 swing forward and backward periodically
-void swingPeriodically(UsbCAN& canII)
-{
-    VCI_CAN_OBJ can;
-    int step = 1;
-    int pwmValue[4] {128,128,135,140};
-
-    //initialize arm1
-    int arm1Value[] {127,255,50,125,235,170,128};
-    generateFrame(can,arm1Value,7,1);
-    canII.transmit(&can,1);
-
-    while(1)
-    {
-        if(pwmValue[0] > 130 || pwmValue[0] <10)
-            step = -step;
-        
-        pwmValue[0]+=step;
-        generateFrame(can,pwmValue,4);
-        canII.transmit(&can,1);
-
-        usleep(10*1000);
-    }
-}
-
-//  It sends data to stm32 via CAN bus to make a single
-//move for the robotic arms.
-void singleMove(UsbCAN& canII)
-{
-    //Arm0
-    // vector<int> pwmValue0 {128,128,135,140};
-    // vector<int> newValue0 {128,128,135,140};
-    //Arm1
-    // vector<int> pwmValue1 {127,255,50,125,235,170,128};
-    vector<int> newValue1;
-
-    //initialize
-    reset2initPos(newValue1,canII,1);
-    // fixStepMove(pwmValue0,newValue0,canII,0);
-    // fixStepMove(pwmValue1,newValue1,canII,1);
-    
-    while(1)
-    {
-        cin >> newValue1[1] >> newValue1[2];
-        fixStepMove(newValue1,canII,1);
-    }
-}
-
-//block and receive msg from CAN
-void receiveFromCAN(UsbCAN& canII)
-{
-    VCI_CAN_OBJ can[50];
-
-    while(1)
-    {
-        int count = canII.receive(can,50);
-        if(!count)
-        {
-            continue;
-        }
-
-        printf("count is %d\n",count);
-        for(int i=0;i<count;i++)
-        {
-            unsigned char len = can[i].DataLen;
-            printf("ID is %011x\n",can[i].ID);
-            printf("data len %d\n",len);
-            printf("timestamp %d\n",can[i].TimeStamp);
-            for(int j = 0;j<len;j++)
-            {
-                printf("%02d ",can[i].Data[j]);
-            }
-            printf("\n\n");
-        }
-    }
-}
-
-
-//use matlab network to predict
-void matlabPredict(UsbCAN& canII)
-{
-    //! Network parameter
-    string dataPath = "../data/network_data.txt";
-    int buffSize = 16*20;
-    
-    MatlabNetwork network(6);
-    network.loadParams(dataPath,buffSize);
-    Vec3d input;
-
-    vector<int> pwmValue {128,128,135,140};
-
-    while(1)
-    {
-        cin >> input[0] >> input[1] >> input[2];
-        Mat output = network.predict(input);
-        vector<int> newValue{
-            (int)output.at<double>(0),
-            (int)output.at<double>(1),
-            135,140};
-        cout<<"calculate:"<<output<<endl;
-        
-        fixStepMove(newValue,canII,0);
-    }
-}
-
-
-//use tensorflow network to predict
-void tfPredict(UsbCAN& canII)
-{
-    //! Network parameter
-    string modulePath = "/home/savage/workspace/cpp_ws/Aruco-marker/src";
-    string moduleName = "tf_network";
-    string funcName   = "main";
-
-    vector<double> inout{0.,0.};
-    TfNetwork network(modulePath,moduleName,funcName);
-
-    //Arm1
-    vector<int> newValue1;
-    reset2initPos(newValue1,canII,1);
-
-    while(1)
-    {
-        cin >> inout[0] >> inout[1];
-        network.callFunction(inout,inout);
-        newValue1[1] = (int)inout[0];
-        newValue1[2] = (int)inout[1];
-
-        cout<<"calculated motor value:"<<newValue1[1]<<"  "<<newValue1[2]<<endl;
-        
-        fixStepMove(newValue1,canII,1);
-    }
-}
+static void* camera_thread(void* data);
 
 
 //camera instrinc matrix and distort coefficients
@@ -227,40 +42,17 @@ ArucoMarker m2Marker1(vector<int>({4}), M2_cameraMatrix1, M2_distCoeffs1);
 ArucoMarker m2Marker2(vector<int>({4}), M2_cameraMatrix1, M2_distCoeffs1);
 RsVideoCapture camera_rs;
 
-//camera thread for func `moveInRoute`
-static void* camera_thread(void* data)
+
+#define _DEBUG_MODE_
+
+int main()
 {
-    Mat img0, img1, img2;
-    VideoCapture camera1(4);
-    VideoCapture camera2(3);
-    namedWindow("view_front", WINDOW_AUTOSIZE);
-    namedWindow("view_arm", WINDOW_AUTOSIZE);
-    namedWindow("view_up", WINDOW_AUTOSIZE);
-    
-    while((char)waitKey(20) != 'q')
+    UsbCAN canII;
+    if(canII.initCAN(UsbCAN::BAUDRATE_500K))
     {
-        camera_rs >> img0;
-        camera1 >> img1;
-        camera2 >> img2;
-        m2Marker0.detect(img0);
-        m2Marker0.outputOffset(img0,Point(30,30));
-        m2Marker1.detect(img1);
-        m2Marker1.outputOffset(img1,Point(30,30));
-        m2Marker2.detect(img2);
-        m2Marker2.outputOffset(img2,Point(30,30));
-        imshow("view_front",img0);
-        imshow("view_arm",img1);
-        imshow("view_up",img2);
+        cout<<"init successfully\n"
+            <<"transmitting..."<<endl;
     }
-
-    pthread_exit(0);
-}
-
-// #define _DEBUG_MODE_
-
-//move along a certain route
-void moveInRoute(UsbCAN& canII)
-{
 
     //create camera thread
     pthread_t cameraThread;
@@ -269,7 +61,8 @@ void moveInRoute(UsbCAN& canII)
     //Arm1 initialize
     vector<int> newValue1;
     reset2initPos(newValue1,canII,1);
-    cin.get();cin.get();
+    cout << "press <enter> if you get ready" << endl;
+    cin.get();
 
     //! Network parameter
     string modulePath = "/home/savage/workspace/cpp_ws/Aruco-marker/src";
@@ -309,26 +102,26 @@ void moveInRoute(UsbCAN& canII)
         move2desiredPos(targetPos[0],obstacleH-diff5_8-30,
                     newValue1,network,canII,1);
     }
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
+    #endif
 
     //above the target, for more precise adjustment
     int x_offset = 10;
     move2desiredPos(targetPos[0]+x_offset,targetPos[1]-diff5_8-25,
                     newValue1,network,canII,1);
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
+    #endif
 
     //motor #1 roughly move (rotate)
     newValue1[0] = -90.91 * motor1angle + 127;
     fixStepMove(newValue1,canII,1);
     cout << motor1angle << endl;
     
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
+    #endif
     
     //adjust motor #6 (rotate)
     while(true)
@@ -367,9 +160,9 @@ void moveInRoute(UsbCAN& canII)
             usleep(20*1000);
         }
     }
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
+    #endif
 
     //adjust motor #4(prepare for dig-into step)
     while(true)
@@ -401,18 +194,17 @@ void moveInRoute(UsbCAN& canII)
             }
         }//end if(m2Marker1.index(4) != -1)
     }
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
-
+    #endif
 
 
     //dig into the target
     move2desiredPos(targetPos[0]+x_offset,targetPos[1]-diff5_8-5,
                     newValue1,network,canII,1);
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
+    #endif
 
     //adjust motor #5 to vertical direction
     cout<<"start to adjust #8...\n";
@@ -443,9 +235,9 @@ void moveInRoute(UsbCAN& canII)
             }
         }//end if(index8 != -1)
     }
-#ifdef _DEBUG_MODE_
+    #ifdef _DEBUG_MODE_
     cin.get();
-#endif
+    #endif
 
     //use #7 to grab the cube
     cout<<"start to grab the cube\n";
@@ -473,5 +265,37 @@ void moveInRoute(UsbCAN& canII)
 
     //wait...
     pthread_join(cameraThread, NULL);
+    
+    return 0;
+}
 
+
+
+//camera thread for func `moveInRoute`
+static void* camera_thread(void* data)
+{
+    Mat img0, img1, img2;
+    VideoCapture camera1(4);
+    VideoCapture camera2(3);
+    namedWindow("view_front", WINDOW_AUTOSIZE);
+    namedWindow("view_arm", WINDOW_AUTOSIZE);
+    namedWindow("view_up", WINDOW_AUTOSIZE);
+    
+    while((char)waitKey(20) != 'q')
+    {
+        camera_rs >> img0;
+        camera1 >> img1;
+        camera2 >> img2;
+        m2Marker0.detect(img0);
+        m2Marker0.outputOffset(img0,Point(30,30));
+        m2Marker1.detect(img1);
+        m2Marker1.outputOffset(img1,Point(30,30));
+        m2Marker2.detect(img2);
+        m2Marker2.outputOffset(img2,Point(30,30));
+        imshow("view_front",img0);
+        imshow("view_arm",img1);
+        imshow("view_up",img2);
+    }
+
+    pthread_exit(0);
 }
